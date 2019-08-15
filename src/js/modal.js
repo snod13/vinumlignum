187c7080f6b3465e6a94a4b41d3cfacf3ad6ee89
snod13 <a.50r0kin@yandex.ru>
2019-08-15 13:38:46 +0300

    Настройка модального окна

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,13 +1,17 @@
 $(document).ready(function(){
-  $(window).resize(function() {
-    
-  var screenWidth = $(window).width();
-  console.log(screenWidth);
-  if(screenWidth < 1200) {
-    $('.modal').addClass('modal_active');
-  }
-  if (screenWidth > 1200) {
-    $('.modal').removeClass('modal_active');
-  }
+  $(window).load(function(){
+      var screenWidth = $(window).width();
+      if(screenWidth < 1200) {
+        $('.modal').addClass('modal_active');
+      }
+    $(window).resize(function () {
+      var screenWidth = $(window).width();
+      if (screenWidth < 1200) {
+        $('.modal').addClass('modal_active');
+      }
+      if (screenWidth > 1200) {
+        $('.modal').removeClass('modal_active');
+      }
+    })
   })
 });
